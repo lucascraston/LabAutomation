6aa97184e8cb2a0ac952bea376d3d86a364e4a86
lucascraston <lucascraston@gmail.com>
2021-12-22 13:13:06 -0500

    First Commit

---
+++ control_rev0.py
@@ -0,0 +1,103 @@
+import pyvisa
+import sys
+
+
+USB = "USB0::0xF4EC::0xEE38::SDSMMFCX5R3326::INSTR"
+LAN = "TCPIP0::192.168.137.164::inst0::INSTR"
+
+rm = pyvisa.ResourceManager()
+adress = rm.list_resources()
+
+scope = rm.open_resource(LAN)
+print(scope.query("*IDN?"))
+
+
+
+def volt_division(channel,voltdiv):
+    scope.write("{}:VDIV {}V".format(channel,voltdiv))
+    
+def time_division(tdiv):
+        scope.write("TDIV {}S".format(tdiv))
+        
+def trigger_mode(mode):    
+    scope.write("TRMD {}".format(mode))
+    
+def trigger_level(channel,level):
+    scope.write("{}:TRLV {}V".format(channel,level))
+    
+def channel_offset(channel,offset):
+    scope.write("{}:OFST {}".format(channel,offset))
+        
+
+
+C1 = 'C1'
+C2 = 'C2'
+C3 = 'C3'
+C4 = 'C4'
+
+  
+#function_list = ['M','V','T','TM','TL','Q']
+
+dict_function = {'V':volt_division,'T':time_division,'Q':quit,'TM':trigger_mode,'TL':trigger_level,'CO':channel_offset}
+
+measure_parameter = ['PKPK','MAX','MIN','AMPL','TOP','BASE','CMEAN','MEAN', 'STDEV','VSTD', 'RMS','CRMS','OVSN','FPRE','OVSP','RPRE', 'LEVELX','DELAY','TIMEL', 'PER', 'FREQ', 'PWID','NWID', 'RISE','FALL','WID','DUTY','NDUTY','ALL']
+
+
+
+
+
+def get_command():
+    command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")
+
+    while command != 'Q' or command != 'q':
+
+        if command in dict_function.keys():
+            execute = dict_function.get(command)
+         
+        if execute == volt_division:
+            chan_in,voltInput = input("Please choose your channel and Division: ").split(',')
+            volt_division(chan_in,voltInput)
+            command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")
+            
+            
+        if execute == time_division:
+            timeInput = input("Please choose your time Division: ")
+            time_division(timeInput)
+            command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")    
+            
+        if execute == trigger_mode:
+            mode = input("choose your trigger mode; NORM, AUTO, SINGLE: ")
+            trigger_mode(mode)
+            command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")
+            
+        if execute == trigger_level:
+            chan_in,level = input("choose your channel and trigger level: ").split(',')
+            trigger_level(chan_in,level)
+            command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")
+            
+        if execute == channel_offset:
+            chan_in,offset = input("choose your channel and channel offset: ").split(',')
+            channel_offset(chan_in,offset)
+            command = input("\n V)Voltage division T)Time division TM)Trigger Mode TL)Trigger Level CO)Channel Offset Q)uit \n: ")    
+        
+        if execute == quit:
+            quit()   
+        
+get_command()        
+        
+       
+    
+    
+    
+    
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
