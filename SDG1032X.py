7496b302658ec8e6400c6fc60efe4649489327be
Lucas Craston <lucascraston@gmail.com>
2022-10-28 13:28:40 -0400

    added first sdg function

--- SDG1032X.py
+++ SDG1032X.py
@@ -4,4 +4,22 @@ class sdg1032x:
     def __init__(self,adress:str):
         self.rm = pyvisa.ResourceManager()
         self.adress = adress
-        self.scope = self.rm.open_resource(self.adress)
+        self.awg = self.rm.open_resource(self.adress)
+
+
+    def channel_output(self,channel:str,state:str,impedance:str):
+        '''
+        channel: 'C1' or 'C2'
+        state: 'ON' or 'OFF'
+        impedance: 'HZ' or '50'
+        
+        '''
+        self.awg.write("{}:OUTP {},LOAD,{},PLRT,NOR".format(channel,state,impedance))
+
+    def arb_serial(self):
+        return 1    
+
+
+awg = sdg1032x("TCPIP0::10.42.0.107::inst0::INSTR")
+print("connection initiaded")
+awg.channel_output("C1","ON","50")    
